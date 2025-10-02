1857898fb2946ae45267114e7d1b7275776c92cc
oleeriksen <oleeriksen1958@gmail.com>
2025-10-02 09:36:08 +0200

    local storage

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ClientApp;
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// brug af local storage
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
