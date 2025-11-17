4c626b32d606cf011f6c9cd2211c49160d67d450
oleeriksen <oleeriksen1958@gmail.com>
2025-11-17 09:04:52 +0100

    Update PingController.cs

--- ServerApp/Controllers/PingController.cs
+++ ServerApp/Controllers/PingController.cs
@@ -9,6 +9,6 @@ public class PingController : ControllerBase
     [HttpGet]
     public string Ping()
     {
-        return "ServerApp - version 0.1 - 24-11-2026";
+        return "ServerApp - version 1.0 - 2025-11-17";
     }
 }
