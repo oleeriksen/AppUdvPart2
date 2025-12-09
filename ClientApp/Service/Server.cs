32fb7ffa5636cc78d7d7571fbace002d54ddf1ff
oleeriksen <oleeriksen1958@gmail.com>
2025-12-09 11:06:29 +0100

    div

--- ClientApp/Service/Server.cs
+++ ClientApp/Service/Server.cs
@@ -2,5 +2,6 @@ namespace ClientApp.Service;
 
 public class Server
 {
+    //public static string Url = "https://apiserver2025.azurewebsites.net/api";
     public static string Url = "http://localhost:5217/api";
 }
